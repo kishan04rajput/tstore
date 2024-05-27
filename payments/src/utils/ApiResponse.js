9d830b0b101af7cee5f4759dd1e90f5115c252ea
Sandip <smssandip.s1212@gmail.com>
2024-05-27 07:19:30 +0530

    folder structure created

---
+++ payments/src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse { // This is my structure for Response
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
+
+export { ApiResponse }
