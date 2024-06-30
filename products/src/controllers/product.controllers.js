48effd3ed9e5af9e8e3c4b1ab1db82ec0b4b7a2d
Sandip <smssandip.s1212@gmail.com>
2024-06-30 15:38:11 +0530

    added pagination in get products

--- products/src/controllers/product.controllers.js
+++ products/src/controllers/product.controllers.js
@@ -8,48 +8,62 @@ import { authenticateUserByAccessToken } from "../gRPC/client.js";
 
 
 const createProduct = asyncHandler(async (req, res) => {
-    const {title, category, description, price, stock} = req.body
-    const createdProduct = await Product.create({
-        createdBy: new mongoose.Types.ObjectId(req.user.id),
-        title,
-        category,
-        description,
-        price,
-        stock
-    })
-    if (!createdProduct) {
-        throw new ApiError(500, "Unable to create Product");
-    }
+  const { title, category, description, price, stock } = req.body;
+  const createdProduct = await Product.create({
+    createdBy: new mongoose.Types.ObjectId(req.user.id),
+    title,
+    category,
+    description,
+    price,
+    stock,
+  });
+  if (!createdProduct) {
+    throw new ApiError(500, "Unable to create Product");
+  }
 
-    return res
+  return res
     .status(200)
     .json(
-        new ApiResponse(200, {createdProduct}, "Category Created Successfully")
+      new ApiResponse(200, { createdProduct }, "Category Created Successfully"),
     );
 
 });
 
 const createCategory = asyncHandler(async (req, res) => {
-    const {name, description, tags} = req.body
+  const { name, description, tags } = req.body;
 
-    const createdCategory = await Category.create({
-        createdBy: new mongoose.Types.ObjectId(userObj.id),
-        name: name,
-        description: description || '',
-        tags: tags || ''
-    })
+  const createdCategory = await Category.create({
+    createdBy: new mongoose.Types.ObjectId(userObj.id),
+    name: name,
+    description: description || "",
+    tags: tags || "",
+  });
 
-    if(!createdCategory) {
-        throw new ApiError(500, "Unable to create category");
-    }
+  if (!createdCategory) {
+    throw new ApiError(500, "Unable to create category");
+  }
 
-    const category = await Category.findById(createdCategory._id);
-    return res
+  const category = await Category.findById(createdCategory._id);
+  return res
     .status(200)
     .json(
-        new ApiResponse(200, {category}, "Category Created Successfully")
+      new ApiResponse(200, { category }, "Category Created Successfully"),
     );
-    
+
 });
 
-export {createProduct, createCategory};
+const getProducts = asyncHandler(async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const products = await Product.find()
+    .skip((page - 1) * limit)
+    .limit(limit)
+    .lean();
+  const totalDocuments = await Product.countDocuments();
+  const totalPages = Math.ceil(totalDocuments / limit);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { products, totalPages, page, limit }, "Product Fetched"));
+});
+
+export { createProduct, createCategory, getProducts };
--- products/src/routes/product.routes.js
+++ products/src/routes/product.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createCategory, createProduct } from "../controllers/product.controllers.js";
+import { createCategory, createProduct, getProducts } from "../controllers/product.controllers.js";
 import { verifyJWT } from "../middlewares/authentication.middlewares.js";
 
 const productRouter = Router();
 
 productRouter.route("/create-product").post(verifyJWT, createProduct);
 productRouter.route("/create-category").post(verifyJWT, createCategory);
+productRouter.route("/get-products").get(getProducts);
 
 export {productRouter}
